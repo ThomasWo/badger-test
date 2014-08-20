b203999c260d79922bc8b0d4ed146667f0eb6135
Laura Mosher <laura.trevisan@gmail.com>
2014-08-19 21:10:34 -0400

    Events Controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,39 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def new
+    @event = Event.new
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    start_date = end_date = []
+
+    (1..5).each do |i|
+      start_date << params[:event]["start(#{i}i)"].to_i
+      end_date << params[:event]["end(#{i}i)"].to_i
+    end
+
+    @event = Event.create(
+      name: params[:event][:name],
+      start_date: DateTime.civil_from_format(:local, start_date[0], start_date[1], start_date[2], start_date[3], start_date[4]),
+      end_date: DateTime.civil_from_format(:local, end_date[0], end_date[1], end_date[2], end_date[3], end_date[4])
+    )
+
+    redirect_to @event
+  end
+end
